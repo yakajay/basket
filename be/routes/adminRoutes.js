b14c966c2218ff146beb4181e6b7ef99399742ed
Yakkali Ajay Kumar <102503364+yakajay@users.noreply.github.com>
2026-02-16 23:47:06 +0530

    user-routes

--- be/routes/adminRoutes.js
+++ be/routes/adminRoutes.js
@@ -2,6 +2,7 @@ const adminController = require("../controllers/adminController")
 const express = require("express")
 const router = express.Router()
 
+
 router.post("/admin-register", adminController.adminRegister)
 router.post("/login-record", adminController.adminLogin)
 
